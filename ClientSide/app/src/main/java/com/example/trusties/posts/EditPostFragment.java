77772b9a5c1b658822d3d68035e27377644fe025
ortal <ortallik@gmail.com>
2022-06-05 14:47:49 +0300

    Test case - Edit post(title/description/location) in client side

--- ClientSide/app/src/main/java/com/example/trusties/posts/EditPostFragment.java
+++ ClientSide/app/src/main/java/com/example/trusties/posts/EditPostFragment.java
@@ -35,6 +35,7 @@ import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.trusties.CommonFunctions;
 import com.example.trusties.R;
 import com.example.trusties.model.Model;
 import com.google.android.gms.location.FusedLocationProviderClient;
@@ -68,7 +69,6 @@ public class EditPostFragment extends Fragment implements OnMapReadyCallback {
     ProgressBar progressBar;
     ImageView image, image2;
 
-    Spinner dropdown;
     Button saveBtn;
     Button cancelBtn;
     ImageButton cameraBtn;
@@ -92,7 +92,7 @@ public class EditPostFragment extends Fragment implements OnMapReadyCallback {
     private FusedLocationProviderClient fusedLocationProviderClientLocationClient;
     String address;
     String locationSOS = null;
-    int isSOS =0;
+    int isSOS = 0;
 
 
     @Override
@@ -129,20 +129,17 @@ public class EditPostFragment extends Fragment implements OnMapReadyCallback {
 
                 String title = post.get("title").toString().replace("\"", "");
                 String description = post.get("description").toString().replace("\"", "");
-                displayPost(title, description); //TODO: add tags, image etc..
+                displayPost(title, description);
                 progressBar.setVisibility(View.GONE);
                 updateUI(View.VISIBLE);
                 mapView.setVisibility(View.GONE);
-                Log.d("TAG",post.get("role").toString());
+                Log.d("TAG", post.get("role").toString());
                 if (post.get("role").toString().replace("\"", "").equals("SOS")) {
                     mapView.setVisibility(View.VISIBLE);
                     addressTv.setVisibility(View.VISIBLE);
-                    Log.d("TAG","yesyesyes");
                     isSOS = 1;
                     address = post.get("address").toString().replace("\"", "");
                     locationSOS = post.get("location").toString().replace("\"", "");
-
-
                 }
             }
         });
@@ -156,7 +153,7 @@ public class EditPostFragment extends Fragment implements OnMapReadyCallback {
                     @Override
                     public void onComplete(JsonObject post) {
 
-                        savePost(view, titleEt.getText().toString(), descriptionEt.getText().toString(), postId,isSOS);
+                        savePost(view, titleEt.getText().toString(), descriptionEt.getText().toString(), postId, isSOS);
 
                     }
 
@@ -245,13 +242,10 @@ public class EditPostFragment extends Fragment implements OnMapReadyCallback {
         } else if (requestCode == REQUEST_IMAGE_GALLERY) {
             if (resultCode == RESULT_OK) {
                 // Get the Image from data
-                Log.d("TAG", 0 + "");
                 if (data.getClipData() != null) { // multi pics
-                    Log.d("TAG", 1 + "");
                     ClipData mClipData = data.getClipData();
                     int count = data.getClipData().getItemCount();
                     if (count > 2) {
-                        Log.d("TAG", 2 + "");
                         mArrayUri = new ArrayList<Uri>();
                         picFlag = 0;
                         Toast.makeText(getContext(), "you can upload 1 or 2 photos only!", Toast.LENGTH_LONG).show();
@@ -265,7 +259,6 @@ public class EditPostFragment extends Fragment implements OnMapReadyCallback {
                             // adding imageuri in array
                             Uri imageurl = data.getClipData().getItemAt(i).getUri();
                             mArrayUri.add(imageurl);
-                            Log.d("TAG", "IMAGE URI" + imageurl);
                             saveBtn.setEnabled(true);
                         }
                         //setting 1st selected image into image switcher
@@ -274,11 +267,9 @@ public class EditPostFragment extends Fragment implements OnMapReadyCallback {
                             image2.setImageURI(mArrayUri.get(1));
                     }
                 } else if (data.getData() != null) { //only one pic
-                    Log.d("TAG", 4 + "");
                     Uri imageurl = data.getData();
                     mArrayUri.add(imageurl);
                     image.setImageURI(mArrayUri.get(0));
-                    Log.d("TAG", "IMAGE URI" + data.getData());
                     saveBtn.setEnabled(true);
 
                 }
@@ -292,13 +283,17 @@ public class EditPostFragment extends Fragment implements OnMapReadyCallback {
 
     private void savePost(View view, String title, String description, String postId, int isSOS) {
         HashMap<String, String> map = new HashMap<>();
-        map.put("title", title);
-        map.put("description", description);
-        if(isSOS ==1)
-        {
-            map.put("location", locationOnMap.toString());
-            map.put("address", fullAddress);
-        }
+        int isGood = new CommonFunctions().CheckTitleAndDescription(title, description, getContext());
+        if (isGood == 1) {
+            map.put("title", title);
+            map.put("description", description);
+            if (isSOS == 1) {
+                int isLocationFine = new CommonFunctions().CheckLocation(fullAddress, getContext());
+                if (isLocationFine == 1) {
+                    map.put("location", locationOnMap.toString());
+                    map.put("address", fullAddress);
+                }
+            }
 //        if (imageBitmap != null) {
 //            Log.d("TAG", imageBitmap.toString());
 //            Model.instance.encodeBitMapImg(imageBitmap, new Model.encodeBitMapImgListener() {
@@ -309,47 +304,47 @@ public class EditPostFragment extends Fragment implements OnMapReadyCallback {
 //            });
 //
 //        }
-        ArrayList<String> photos = new ArrayList<>();
+            ArrayList<String> photos = new ArrayList<>();
 
-        if (imageBitmap != null) {
-            Log.d("TAG", imageBitmap.toString());
-            Model.instance.encodeBitMapImg(imageBitmap, new Model.encodeBitMapImgListener() {
-                @Override
-                public void onComplete(String url) {
-                    map.put("photo", url);
-                }
-            });
-
-        }
-        if (mArrayUri != null) {
-            Log.d("TAG", map.size() + "   jhhk ");
-
-            flag = 1;
-            for (int i = 0; i < mArrayUri.size(); i++) {
-                try {
-                    Model.instance.encodeBitMapImg(BitmapFactory.decodeStream(getContext().getContentResolver().openInputStream(mArrayUri.get(i))), new Model.encodeBitMapImgListener() {
-                        @Override
-                        public void onComplete(String url) {
-                            photos.add(url);
-                        }
-                    });
-                } catch (FileNotFoundException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-        Model.instance.editPost(map, postId, new Model.editPostListener() {
-            @Override
-            public void onComplete(JsonObject res) {
-                Model.instance.addPhotosToPost(photos, res.get("_id").toString().replace("\"", ""), new Model.addPhotosToPostListener() {
+            if (imageBitmap != null) {
+                Log.d("TAG", imageBitmap.toString());
+                Model.instance.encodeBitMapImg(imageBitmap, new Model.encodeBitMapImgListener() {
                     @Override
-                    public void onComplete() {
-                        Log.d("TAG", "stopppppppppp");
+                    public void onComplete(String url) {
+                        map.put("photo", url);
                     }
                 });
-                Navigation.findNavController(view).navigateUp();
+
             }
-        });
+            if (mArrayUri != null) {
+
+                flag = 1;
+                for (int i = 0; i < mArrayUri.size(); i++) {
+                    try {
+                        Model.instance.encodeBitMapImg(BitmapFactory.decodeStream(getContext().getContentResolver().openInputStream(mArrayUri.get(i))), new Model.encodeBitMapImgListener() {
+                            @Override
+                            public void onComplete(String url) {
+                                photos.add(url);
+                            }
+                        });
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            Model.instance.editPost(map, postId, new Model.editPostListener() {
+                @Override
+                public void onComplete(JsonObject res) {
+                    Model.instance.addPhotosToPost(photos, res.get("_id").toString().replace("\"", ""), new Model.addPhotosToPostListener() {
+                        @Override
+                        public void onComplete() {
+                            Log.d("TAG", "stopppppppppp");
+                        }
+                    });
+                    Navigation.findNavController(view).navigateUp();
+                }
+            });
+        }
 
     }
 
@@ -376,7 +371,7 @@ public class EditPostFragment extends Fragment implements OnMapReadyCallback {
     @Override
     public void onMapReady(@NonNull GoogleMap googleMap) {
 
-        if(isSOS == 1) {
+        if (isSOS == 1) {
 
             if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
                     && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
@@ -431,10 +426,7 @@ public class EditPostFragment extends Fragment implements OnMapReadyCallback {
                                 } catch (IOException e) {
                                     e.printStackTrace();
                                 }
-
-
                                 locationOnMap = latLng;
-
                             }
                         });
 
