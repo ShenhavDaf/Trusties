319312c26712770ca4288f3bb6361ca4cb2c6d4e
adiamoyal <adi.amoyal02@gmail.com>
2022-06-12 12:28:21 +0300

    Notifications - add notification for comment, friend request, volunteer for help and approve volunteer

--- ServerSide/Controllers/notifications.js
+++ ServerSide/Controllers/notifications.js
@@ -3,7 +3,7 @@ const Post = require("../Models/post_model");
 const Sos = require("../Models/sos_model");
 const Comment = require("../Models/comment_model");
 const Notification = require("../Models/notification_model");
-const { object } = require("mongoose/lib/utils");
+const { object, getValue } = require("mongoose/lib/utils");
 const request = require("request");
 const admin = require("firebase-admin");
 const serviceAccount = require("../trusties-firebase-adminsdk.json");
@@ -17,19 +17,67 @@ const getAllNotifications = async (req, res, next) => {
   });
 };
 
+const getAllNotificationsByID = async (req, res, next) => {
+  console.log("get all notifications by ID");
+  try {
+    Post.find({ sender: req.params.id }, function (err, postDocs) {
+      if (err) console.log(err);
+      else {
+        Notification.find({}, function (err, notificationDocs) {
+          const resList = [];
+          for (let i = 0; i < notificationDocs.length; i++) { 
+            if(notificationDocs[i]["type"] === "friendRequest") {
+              const post = notificationDocs[i]["post"];
+              if(req.params.id === post.toString()) {
+                resList.push(notificationDocs[i]);
+                continue;
+              }
+            } if(notificationDocs[i]["type"] === "sos") {
+              console.log("SOS -- Notification");
+              continue;
+            } else {
+              const post = notificationDocs[i]["post"];
+              for (let j = 0; j < postDocs.length; j++) {
+                  if(postDocs[j]._id.toString() === post.toString()) {
+                    resList.push(notificationDocs[i]);
+                  }
+              }
+            }
+          }
+          if (err) console.log(err);
+          else res.status(200).send(resList);
+        });
+      }
+    });
+
+  
+  } catch (err) {
+    res.status(400).send({
+      status: "fail",
+      error: err.message,
+    });
+  }
+};
+
 const addNotification = async (req, res, next) => {
   console.log("add new notification ");
+  let post;
+  if(req.body.type === "friendRequest") {
+    post = await User.findOne({ _id: req.body.post });
+  } else if(req.body.type === "sos") {
+    post = "";
+  } else {
+    post = await Post.findOne({ _id: req.body.post });
+  }
 
-  const post = await Post.findOne({ _id: req.body.post });
-  const user = await User.findOne({ email: req.body.sender });
+  const user = await User.findOne({ _id: req.body.sender });
   const time = req.body.time;
   const type = req.body.type;
   const circle = req.body.circle;
 
-
   const notification = Notification({
     sender: user,
-    post: post,
+    post: null,
     time: Date.now(),
     type: type,
     circle: circle
@@ -51,75 +99,76 @@ const addNotification = async (req, res, next) => {
 };
 
 // ## New version - Not fully Working. 
-const sendNotification = async (req, res, next) => {
-  const post = await Post.findOne({ _id: req.body.post });
-  const token_db = (await User.findOne({ _id: post.sender })).firebaseToken;
-
-  const token = [token_db];
-
-
-  const options = {
-    priority: "high",
-    timeToLive: 60 * 60 * 24
-  };
-
-  const payload = {
-    notification: {
-      title: "Test Notification",
-      body: "Hey..."
-    }
-  }
-  admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount)
-  });
-
-  admin.messaging().sendToDevice(token, payload, options)
-    .then(response => {
-
-      console.log("## Inside admin.messaging()");
-      console.log(response);
-      res.status(200).send("OK");
-
-    })
-    .catch(error => {
-      console.log(error);
-    });
-  // request.post({
-  //   headers: { 'content-type': 'application/json', "Authorization": process.env.FIREBASE_TOKEN },
-  //   url: "https://fcm.googleapis.com/fcm/send",
-  //   body: payload,
-  //   json: true,
-  // });
-};
-
-// ## Old version
-
 // const sendNotification = async (req, res, next) => {
-//   console.log("Send notification");
-//   console.log(req.body);
 //   const post = await Post.findOne({ _id: req.body.post });
-//   const token = (await User.findOne({ _id: post.sender })).firebaseToken;
-//   console.log("token: " + token);
+//   const token_db = (await User.findOne({ _id: post.sender })).firebaseToken;
+
+//   const token = [token_db];
+
+
+//   const options = {
+//     priority: "high",
+//     timeToLive: 60 * 60 * 24
+//   };
 
 //   const payload = {
-//     to: token,
 //     notification: {
 //       title: "Test Notification",
 //       body: "Hey..."
 //     }
 //   }
-
-//   request.post({
-//     headers: { 'content-type': 'application/json', "Authorization": process.env.FIREBASE_TOKEN },
-//     url: "https://fcm.googleapis.com/fcm/send",
-//     body: payload,
-//     json: true,
+//   admin.initializeApp({
+//     credential: admin.credential.cert(serviceAccount)
 //   });
+
+//   admin.messaging().sendToDevice(token, payload, options)
+//     .then(response => {
+
+//       console.log("## Inside admin.messaging()");
+//       console.log(response);
+//       res.status(200).send("OK");
+
+//     })
+//     .catch(error => {
+//       console.log(error);
+//     });
+//   // request.post({
+//   //   headers: { 'content-type': 'application/json', "Authorization": process.env.FIREBASE_TOKEN },
+//   //   url: "https://fcm.googleapis.com/fcm/send",
+//   //   body: payload,
+//   //   json: true,
+//   // });
 // };
+
+// ## Old version
+
+const sendNotification = async (req, res, next) => {
+  console.log("Send notification");
+  console.log(req.body);
+  const post = await Post.findOne({ _id: req.body.post });
+  const token = (await User.findOne({ _id: post.sender })).firebaseToken;
+  console.log("token: " + token);
+
+  const payload = {
+    to: token,
+    notification: {
+      title: "Test Notification",
+      body: "Hey..."
+    }
+  }
+
+  request.post({
+    headers: { 'content-type': 'application/json', "Authorization": process.env.FIREBASE_TOKEN },
+    url: "https://fcm.googleapis.com/fcm/send",
+    body: payload,
+    json: true,
+  },(res)=> { console.log(res)});
+};
 
 
 module.exports = {
   getAllNotifications,
+  getAllNotificationsByID,
   addNotification,
   sendNotification
 };
